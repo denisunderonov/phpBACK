e24ec87120f8c2a755e865dbe4df42a40e213601
Denis Andronov <denisunderonov@gmail.com>
2024-02-06 19:55:14 +0300

    add lab1

---
+++ lab1/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello world</title>
+</head>
+<body>
+    <header>
+
+    </header>
+    <main>
+        <?php 
+            echo 'Hello lab 1';
+        ?>
+    </main>
+    <footer>
+        <h2>Андронов Денис Андреевич</h2>
+    </footer>
+</body>
+</html>
